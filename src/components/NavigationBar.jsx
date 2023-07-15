8dc6b25205455048efe5eb158368dbb1eeb56998
Rohit Bhatt <rbhatt199924@gmail.com>
2023-07-16 01:07:31 +0530

    update navigation scroll

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,27 +2,14 @@ import React from 'react'
 import { FiMail } from "react-icons/fi";
 import { BsTelephoneFill } from "react-icons/bs";
 import { AnimatePresence, motion } from 'framer-motion';
+import { Link } from 'react-scroll';
 
 export default function NavigationBar() {
   const [show, setShow] = React.useState(false);
-  const [menuItems, setMenuItems] = React.useState([{ name: 'Home', link: '#', active_status: true }, { name: 'Menu & Pricing', link: '#', active_status: false }, { name: 'How we work?', link: '#', active_status: false }, { name: 'Why us?', link: '#', active_status: false }, { name: 'Contact Us', link: '#', active_status: false }]);
+  const menuItems = [{ name: 'Home', link: 'home' }, { name: 'Menu & Pricing', link: 'menu' }, { name: 'How we work?', link: 'work' }, { name: 'Why us?', link: 'why_us' }, { name: 'Contact Us', link: 'contact' }];
   const handleHamburger = () => {
     setShow(!show);
   }
-
-  const handleNavlinks = (index) => {
-    let temp = [...menuItems];
-    temp.forEach((item, i) => {
-      if (i === index) {
-        item.active_status = true;
-      } else {
-        item.active_status = false;
-      }
-    })
-    setMenuItems(temp);
-    setShow(false);
-  }
-
 
   return (
     <div className='fixed top-0 right-0 left-0 bg-white z-50'>
@@ -43,9 +30,11 @@ export default function NavigationBar() {
       <div className="flex flex-row justify-between items-center px-5 lg:px-20 py-4 shadow-md">
         <h1 className="text-4xl font-bold text-red-700">momimeals</h1>
         <div className=" hidden lg:flex flex-col lg:flex-row gap-10 items-center">
-          {menuItems.map((item, index) => (
-            <button key={index} className={`text-black text-xl box-border py-2 ${item.active_status ? 'active' : ''} transition-all duration-200`} onClick={() => handleNavlinks(index)}>{item.name}</button>
-          ))}
+          <Link activeClass='active' spy={true} to={menuItems[0].link} smooth={true} duration={1000} offset={-150} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[0].name}</Link>
+          <Link activeClass='active' spy={true} to={menuItems[1].link} smooth={true} duration={1000} offset={-100} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[1].name}</Link>
+          <Link activeClass='active' spy={true} to={menuItems[2].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[2].name}</Link>
+          <Link activeClass='active' spy={true} to={menuItems[3].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[3].name}</Link>
+          <Link activeClass='active' spy={true} to={menuItems[4].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[4].name}</Link>
         </div>
         <div className="cursor-pointer lg:hidden flex flex-col gap-1.5 items-center z-50 justify-center" onClick={handleHamburger}>
           <span className={`w-8 border-b-4 border-gray-600 rounded-sm transition-all duration-700 ${show ? 'transform rotate-45 translate-y-2.5' : ''}`} />
@@ -54,18 +43,20 @@ export default function NavigationBar() {
         </div>
       </div>
       <AnimatePresence className="overflow-x-hidden">
-      {show && (
-        <motion.div
-          initial={{ opacity: 0, x: -300 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -300 }}
-          transition={{ duration: 0.7 }}
-          className="flex flex-col gap-5 items-center justify-center lg:hidden fixed top-0 left-0 w-full h-screen  z-20 backdrop-blur-md">
-          {menuItems.map((item, index) => (
-            <button key={index} className={`text-black text-xl box-border py-2 ${item.active_status ? 'active' : ''} transition-all duration-200`} onClick={() => handleNavlinks(index)}>{item.name}</button>
-          ))}
-        </motion.div>
-      )}
+        {show && (
+          <motion.div
+            initial={{ opacity: 0, x: -300 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -300 }}
+            transition={{ duration: 0.7 }}
+            className="flex flex-col gap-5 items-center justify-center lg:hidden fixed top-0 left-0 w-full h-screen  z-20 backdrop-blur-md">
+            <Link to={menuItems[0].link} smooth={true} duration={1000} offset={-100} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[0].name}</Link>
+            <Link to={menuItems[1].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[1].name}</Link>
+            <Link to={menuItems[2].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[2].name}</Link>
+            <Link to={menuItems[3].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[3].name}</Link>
+            <Link to={menuItems[4].link} smooth={true} duration={1000} className="text-xl cursor-pointer hover:text-red-700 transition-all duration-700">{menuItems[4].name}</Link>
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
   )
