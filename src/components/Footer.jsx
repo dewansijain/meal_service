c5724168312192484c065872f54fbcb0d085d67c
Rohit Bhatt <rbhatt199924@gmail.com>
2023-07-16 14:44:37 +0530

    add mobile version footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,16 @@ import style from './Footer.module.css'
 export default function Footer() {
   return (
     <div className={style.bgImage}>
-      <div className="px-5 flex pt-32 pb-28 lg:px-20 bg-stone-900/75 flex-col lg:flex-row gap-16 justify-between" >
-        <div className='flex flex-col w-7/12 gap-10 items-center justify-center'>
-          <h2 className='text-white text-left text-5xl font-black self-start'>
+      <div className="px-5 flex lg:pt-32 pt-12 pb-6 lg:pb-28 lg:px-20 bg-stone-900/75 flex-col lg:flex-row gap-5 lg:gap-16 justify-between" >
+        <div className='flex flex-col lg:w-7/12 gap-4 lg:gap-10 items-center justify-center'>
+          <h2 className='text-white text-left text-2xl lg:text-5xl font-black self-start'>
             The Story Behind Momimeals!
           </h2>
-          <p className='text-white text-xl font-normal text-justify'>
+          <p className='text-white text-lg lg:text-xl font-normal text-justify'>
             The story behind Momimeals is rooted in the heartfelt experiences of its founder, Dewanshi Jain. Being a big foodie herself, Dewanshi deeply missed the comforting taste of her mother's home-cooked meals when she ventured into a new city for her education. She realized that she was not alone in this predicament, as many others faced the challenges of finding healthy and affordable food options that could match the love and care put into a mother's cooking. Dissatisfied with the unappealing choices of restaurant food and tasteless mess meals, Dewanshi embarked on a mission to bring the essence of a mother's wholesome and flavorsome dishes to everyone's plate. With Momimeals, she created a unique proposition, offering delicious and nutritious food at affordable prices. This brand is a loving tribute to Dewanshi's cherished mother, ensuring that the joy and nourishment of home-cooked meals are accessible to all.
           </p>
         </div>
-        <div className="px-5 flex flex-col gap-16 items-center" >
+        <div className="lg:px-5 flex flex-col gap-5 lg:gap-16 lg:items-center" >
           <h2 className='text-white text-3xl w-full font-black'>
             Contact Us
           </h2>
