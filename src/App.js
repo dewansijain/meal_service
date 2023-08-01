11d683613bdd4cb702156ef810205fb3d81169ce
Rohit Bhatt <rbhatt199924@gmail.com>
2023-08-01 17:03:28 +0530

    add construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import HowWeWOrk from './components/HowWeWork';
-import MenuPricing from './components/MenuPricing';
-import NavigationBar from './components/NavigationBar';
-import WhyUs from './components/WhyUs';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Home from './components/Home';
+// import HowWeWOrk from './components/HowWeWork';
+// import MenuPricing from './components/MenuPricing';
+// import NavigationBar from './components/NavigationBar';
+// import WhyUs from './components/WhyUs';
 
 function App() {
 
   return (
-    <div className="mt-28">
-      <NavigationBar />
+    <div>
+      {/* <NavigationBar />
       <Home />
       <MenuPricing />
       <HowWeWOrk />
       <WhyUs />
       <Contact />
-      <Footer />
+      <Footer /> */}
+      <div className="flex justify-center items-center h-screen">
+        <h1 className=" text-2xl text-transparent bg-clip-text font-bold md:text-4xl text-center bg-gradient-to-r from-red-700 via-yellow-500 to-green-700 bg-300% animate-gradient">Site is under construction. We will be back soon!</h1>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
+    },
   },
   plugins: [],
 }
